74c54174b04521b4d50d21d3b8f99b8e2eb8d8ed
齊藤元紀 <saitogenki20031104@gmail.com>
2024-09-18 15:29:50 +0900

    [Add] 途中保存

---
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/GenerateHat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateHat : MonoBehaviour
+{
+    private int hatCount;
+
+    private const int HatMax = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int GetRandomValue()
+    {
+        return Random.Range(0, HatMax);
+    }
+
+    private void Generator()
+    {
+        for(int i = 0; i < ; i++)
+        {
+
+        }
+    }
+}
