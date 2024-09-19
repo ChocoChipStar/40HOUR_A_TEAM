26216ef3a467ecaa5695ba8d48de02418c2530ee
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-09-19 13:11:08 +0900

    [Add]チュートリアルシーンに暗転の実装
    フェードアウトしてからシーンを切り替える機能を実装しました。

--- 40HOUR_B_TEAM/Assets/Project/Scripts/Tutorial/TutorialToMainSceneTransition.cs
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/Tutorial/TutorialToMainSceneTransition.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TutorialToMainSceneTransition : MonoBehaviour
 {
+    //フェードアウト用の変数
+    private GameObject fadeOutImage;
+    private Image fadeOut_image;
+    private Color alpha;
+    private bool fadeOutFlag;　
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject fadeOutImage = GameObject.Find("FadeOut");
+        fadeOut_image = fadeOutImage.GetComponent<Image>();
+
+        alpha = new Color(0.0f, 0.0f, 0.0f, 0.002f);
+        fadeOutFlag = false;
     }
 
     // Update is called once per frame
@@ -38,10 +49,31 @@ public class TutorialToMainSceneTransition : MonoBehaviour
         {
 
             //enterキーかコントローラーのBボタンが押された瞬間に
-            //メインシーンをロード
-            SceneManager.LoadScene("MainScene");
-
+            //フェードアウト開始
+            fadeOutFlag = true;
         }
 
+        if (fadeOutFlag == true && fadeOut_image.color.a < 1)
+            {
+            //フェードアウト
+                FadeOut();
+            }
+
+            
+            if (fadeOut_image.color.a >= 1)
+            {
+            //画面が暗くなったらメインシーンをロード
+
+            SceneManager.LoadScene("MainScene");
+
+            }
+
+    }
+
+    void FadeOut()
+    {
+        //イメージの透明度を少しずつ下げる
+            fadeOut_image.color += alpha;
+        
     }
 }
