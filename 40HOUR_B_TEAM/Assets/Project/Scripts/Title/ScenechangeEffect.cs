0a601a81ac4d527e43b7618731db4ba9da9e32df
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-09-20 15:24:47 +0900

    [Add]タイトルシーンにBGM再生機能とオーディオフェードアウトの実装
    BGMを追加しました。
    スクリプト「ScenechangeEffect」にBGMのフェードアウト機能を実装しました。

--- 40HOUR_B_TEAM/Assets/Project/Scripts/Title/ScenechangeEffect.cs
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/Title/ScenechangeEffect.cs
@@ -5,27 +5,32 @@ using UnityEngine.UI;
 
 public class ScenechangeEffect : MonoBehaviour
 {
+    //フェードアウトのイメージ用のコンポーネント
     [SerializeField]
     private Sprite[] fadeOutImage;
-
     [SerializeField]
     private Image image;
 
+    //BGM調整用のコンポーネント
+    [SerializeField]
+    private AudioSource bgmAudioSource;
+
+    //フェードアウト速度調整用の変数
     int counter;
     public int switchCount;
+
 
     // Start is called before the first frame update
     void Start()
     {
         counter = 0;
         switchCount = 0;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
 
     }
 
@@ -38,15 +43,20 @@ public class ScenechangeEffect : MonoBehaviour
 
             if (switchCount <= 58 && counter == 2)
             {
+                //画像を表示
                 image.sprite = fadeOutImage[switchCount];
                 switchCount++;
+
+                //BGMのボリュームを下げる
+                bgmAudioSource.volume -= 0.02f;
             }
 
 
             counter = 0;
-        }
 
-        Debug.Log(counter);
-        Debug.Log(switchCount);
+            
+        }
+      
     }
+
 }
