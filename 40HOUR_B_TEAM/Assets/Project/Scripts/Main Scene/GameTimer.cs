a0c754574387f8a507b6f698a1cbaeaf2b13e3c1
Sasaki Yu <yusasaki34@icloud.com>
2024-09-20 10:11:36 +0900

    [Add]メインシーンの残り時間タイマーを追加
    10秒からカウントダウンが始まり０がしばらく表示されたら非表示

---
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/Main Scene/GameTimer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    float timer;
+    bool timerStop;
+
+    //フラグとタイマー時間を初期化
+    void Start()
+    {
+        timer = 10.9f;
+        timerStop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //0になったらコンポーネントを非表示
+        if (timerStop)
+        {
+            timerText.enabled = false;
+        }
+        //変数をTMPに表示
+        timerText.text = Mathf.FloorToInt(timer).ToString();
+
+        //0になるまで減少
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timerStop = true;
+        }
+    }
+}
