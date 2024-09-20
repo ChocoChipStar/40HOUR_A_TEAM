5d94f69ea2cc679bf2e2dec90e29f08676f79e29
齊藤元紀 <saitogenki20031104@gmail.com>
2024-09-20 14:00:43 +0900

    [Update] 途中保存

--- 40HOUR_B_TEAM/Assets/Project/Scripts/HatData.cs
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/HatData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class HatData
 {
-    private const int generatePointCount = 5;
+    private const int GeneratePointMax = 5;
 
     public static readonly int KingHatGenerateRound;
 
@@ -10,17 +10,26 @@ public static class HatData
 
     public static readonly Vector3[,] GeneratePosition;
 
+    public const int RoundMax = 4;
+
     public const int HatMax = 10;
 
     static HatData()
     {
         KingHatGenerateRound = 3;
 
-        GeneratePosition = new Vector3[generatePointCount];
-        GeneratePosition[0] = new Vector3(-12.0f, 2.8f, -28.0f);
-        GeneratePosition[1] = new Vector3(-6.0f,  2.8f, -28.0f);
-        GeneratePosition[2] = new Vector3( 0.0f,  2.8f, -28.0f);
-        GeneratePosition[3] = new Vector3( 6.0f,  2.8f, -28.0f);
-        GeneratePosition[4] = new Vector3( 12.0f, 2.8f, -28.0f);
+        GeneratePosition = new Vector3[RoundMax, GeneratePointMax];
+
+        GeneratePosition[0,0] = new Vector3(-12.0f, 2.8f, -28.0f);
+        GeneratePosition[0,1] = new Vector3( -6.0f, 2.8f, -28.0f);
+        GeneratePosition[0,2] = new Vector3(  0.0f, 2.8f, -28.0f);
+        GeneratePosition[0,3] = new Vector3(  6.0f, 2.8f, -28.0f);
+        GeneratePosition[0,4] = new Vector3( 12.0f, 2.8f, -28.0f);
+
+        GeneratePosition[1,0] = new Vector3(-12.0f, 2.8f, -28.0f);
+        GeneratePosition[1,1] = new Vector3( -6.0f, 2.8f, -28.0f);
+        GeneratePosition[1,2] = new Vector3(  0.0f, 2.8f, -28.0f);
+        GeneratePosition[1,3] = new Vector3(  6.0f, 2.8f, -28.0f);
+        GeneratePosition[1,4] = new Vector3( 12.0f, 2.8f, -28.0f);
     }
 }
