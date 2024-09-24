a8e33fbbeffd4822da085100467a0e49ff246f26
kohnokouta <xiangyehangda@gmail.com>
2024-09-24 15:19:11 +0900

    [add]モデル追加

--- 40HOUR_B_TEAM/Assets/Project/Scripts/Result/RankingPlayer.cs
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/Result/RankingPlayer.cs
@@ -1,37 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class RankingPlayer : MonoBehaviour
 {
+
     [SerializeField]
-    public int ranking;
+    private RankingUI rankingUI;
 
     [SerializeField]
     private Animator animator;
+
+    [SerializeField]
+    private Material material;
+
+    [SerializeField]
+    private Texture smileSTexture;
+
+    [SerializeField]
+    private Texture smileLTexture;
+
+    [SerializeField]
+    private Texture shockTexture;
+
+    private int playerCount;
+
     void Start()
     {
-        
+        playerCount = 0;
+        material.mainTexture = smileSTexture;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerRankingPosition(int count)
     {
-        if (ranking == 1)
+        if (count == 1)
         {
-            animator.Play("run");
+            gameObject.transform.position = new Vector3(-5.5f, -1.3f, 0);
         }
-        else if (ranking == 2)
+        else if (count == 2)
         {
-            animator.Play("Decition");
+            gameObject.transform.position = new Vector3(-1.833333f, -1.3f, 0);
         }
-        else if (ranking == 3)
+        else if (count == 3)
         {
-            animator.Play("Think");
+            gameObject.transform.position = new Vector3(1.833333f, -1.3f, 0);
         }
         else
         {
-            animator.Play("Stand");
+            gameObject.transform.position = new Vector3(5.5f, -1.3f, 0);
+        }
+
+        playerCount = count;
+    }
+
+    public void PlayerRankReaction(int rank)
+    {
+        if (rank == 1)
+        {
+            material.mainTexture = smileLTexture;
+            rankingUI.RankingUIEnabled(rank);
+            rankingUI.RankingUIPosition(playerCount);
+            animator.Play("1st");
+        }
+        else if (rank == 2)
+        {
+            material.mainTexture = smileLTexture;
+            rankingUI.RankingUIEnabled(rank);
+            rankingUI.RankingUIPosition(playerCount);
+            animator.Play("2st");
+        }
+        else if (rank == 3)
+        {
+            material.mainTexture = smileSTexture;
+            rankingUI.RankingUIEnabled(rank);
+            rankingUI.RankingUIPosition(playerCount);
+            animator.Play("3st");
+        }
+        else
+        {
+            material.mainTexture = shockTexture;
+            rankingUI.RankingUIEnabled(rank);
+            rankingUI.RankingUIPosition(playerCount);
+            animator.Play("4st");
         }
     }
 }
