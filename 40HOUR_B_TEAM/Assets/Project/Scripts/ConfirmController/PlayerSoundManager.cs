b3f57ba4cb4ebb4febff26c160384b5f9bc97cb4
kohnokouta <xiangyehangda@gmail.com>
2024-09-24 18:31:45 +0900

    [Add]アニメーションを追加

---
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/ConfirmController/PlayerSoundManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    //よし
+    [SerializeField] AudioClip Playercheck_Good;
+    
+    //ホワッツ？
+    [SerializeField] AudioClip Playercheck_What;
+    
+    //えー？
+    [SerializeField] AudioClip Playercheck_Surprise;
+    
+    //シャキーン
+    [SerializeField] AudioClip Playercheck_Shakeen;
+
+    [SerializeField] AudioClip Playercheck_OK;
+
+    public void PlayerSE()
+    {
+        audioSource.Play();
+    }
+
+    public void GoodSoundChange()
+    {
+        audioSource.clip = Playercheck_Good;
+    }
+
+    public void WhatSoundChange()
+    {
+        audioSource.clip = Playercheck_What;
+    }
+
+    public void SurpriseSoundChange()
+    {
+        audioSource.clip = Playercheck_Surprise;
+    }
+
+    public void ShakeenSoundChange()
+    {
+        audioSource.clip = Playercheck_Shakeen;
+    }
+
+    public void OKSoundChange()
+    {
+        audioSource.clip = Playercheck_OK;
+    }
+}
+
--- 40HOUR_B_TEAM/Assets/Project/Scripts/Player/Player.cs
+++ 40HOUR_B_TEAM/Assets/Project/Scripts/Player/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] int gamePadCount;
     [SerializeField] SpriteRenderer okTextSprite;
     [SerializeField] Animator animator;
+    [SerializeField] PlayerSoundManager soundManager;
     private Gamepad gamepads;    
 
 
@@ -25,23 +26,33 @@ public class Player : MonoBehaviour
 
         if (gamepads.aButton.wasPressedThisFrame)
         {
+            soundManager.ShakeenSoundChange();
+            soundManager.PlayerSE();
             animator.Play("anime_Happy");
         }
         if (gamepads.bButton.wasPressedThisFrame)
         {
+            soundManager.WhatSoundChange();
+            soundManager.PlayerSE();
             animator.Play("anime_YareYare");
         }
         if(gamepads.xButton.wasPressedThisFrame)
         {
+            soundManager.GoodSoundChange();
+            soundManager.PlayerSE();
             animator.Play("anime_Decition");
         }
         if (gamepads.yButton.wasPressedThisFrame)
         {
+            soundManager.SurpriseSoundChange();
+            soundManager.PlayerSE();
             animator.Play("anime_Shock");
         }
 
         if (gamepads.leftShoulder.isPressed && gamepads.rightShoulder.isPressed)
         {
+            soundManager.OKSoundChange();
+            soundManager.PlayerSE();
             okTextSprite.enabled = true;
         }
     }
